df22c3f8caf50d4562e6467c77ce8ccb1b89eb8d
yuanzhoucq <yuanzhou.cq@gmail.com>
2017-09-10 00:39:54 +0200

    init

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask
+from flask.ext import restful
 
 app = Flask(__name__)
+api = restful.Api(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+class HelloWorld(restful.Resource):
+    def get(self):
+        return {'hello': 'world'}
 
+
+api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
