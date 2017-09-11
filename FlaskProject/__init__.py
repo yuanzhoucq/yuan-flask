64fcdcb67c173b6c8947877552ec958a6b267b35
yuanzhoucq <yuanzhou.cq@gmail.com>
2017-09-11 23:37:54 +0200

    add CORS

--- FlaskProject/__init__.py
+++ FlaskProject/__init__.py
@@ -1,5 +1,7 @@
+import json
+
 import flask_restful as restful
-from flask import Flask, redirect
+from flask import Flask, redirect, make_response
 
 from FlaskProject.Transpole import Transpole
 
@@ -19,6 +21,15 @@ class HelloWorld(restful.Resource):
 
 api.add_resource(HelloWorld, '/api/')
 api.add_resource(Transpole, '/api/transpole/<string:line>/<string:direction>/<int:start_station>')
+
+
+@api.representation('application/json')
+def output_json(data, code, headers=None):
+    """add the CORS"""
+    resp = make_response(json.dumps(data), code)
+    resp.headers.extend(headers or {})
+    resp.headers.extend({'Access-Control-Allow-Origin': '*'})
+    return resp
 
 if __name__ == '__main__':
     app.run(debug=True)
